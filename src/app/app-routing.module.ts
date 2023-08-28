4744767eba94e6d100eaf2c777cdad2054fb1f0d
ErlingMMM <emagnusmoland@gmail.com>
2023-08-28 14:12:09 +0200

    update for routinbg module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPage } from './pages/landing/landing.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: '', component: TrainerPage
+},
+{
+  path: 'login', component: LandingPage
+},
+{
+  path: 'pokemons', component: PokemonCataloguePage
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
