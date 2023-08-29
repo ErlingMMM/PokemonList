aef6f0365adb91011ff78756d1cc0cc890d630d4
ErlingMMM <emagnusmoland@gmail.com>
2023-08-29 18:14:52 +0200

    div for marked pokemon as saved

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -1,5 +1,6 @@
 export interface Pokemon {
   name: string;
   image: string;
+  catched: boolean;
 }
 
--- src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
+++ src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { Pokemon } from 'src/app/models/pokemon';
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -12,7 +14,10 @@ export class PokemonCataloguePage implements OnInit {
 
   pokemons: Pokemon[] = [];
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(
+    private pokemonService: PokemonService,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
     this.getPokemonList();
@@ -24,15 +29,38 @@ export class PokemonCataloguePage implements OnInit {
         this.pokemons = pokemonList.map((pokemon, index) => ({
           ...pokemon,
           image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+          catched: false
         }));
         console.log('Fetched Pokemons:', this.pokemons);
       });
   }
 
   saveIndexAndName(name: string, index: number): void {
-    console.log('Clicked on Pokémon with index:', name);
-    console.log(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`);
+    console.log('Clicked on Pokémon with name:', name);
 
+    const data = {
+      name: name,
+      index: index + 1,
+    };
+
+    this.http.post("https://assigment2-api-production.up.railway.app/trainers", data, {
+      headers: {
+        'Authorization': `Bearer jP9kL7Hn3RmTqAeWsZcXvYbUgIaOpEfDhCtVrFmNlYbUiTsWxZaQpOeHdCfGjK`
+      }
+    }).subscribe(
+      response => {
+        console.log('API Response:', response);
+      },
+      error => {
+        console.error('Error:', error);
+      }
+    );
   }
-  
+
+  markAsCatched(pokemon: Pokemon, index: number): void {
+    console.log('Marked as catched:', pokemon.name);
+    console.log('Index of the Pokémon:', index);
+    pokemon.catched = true; 
+  }
+
 }
